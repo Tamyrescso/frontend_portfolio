25dba155a5bbbe6d9a04ed2e6d77774939650229
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-05 22:12:29 -0300

    altera App.js

---
+++ portifolio/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Abilities from './pages/Abilities';
+import NotFound from './pages/NotFound';
+import './style/app.css';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route exact path="/projects" element={ <Projects/> } />
+      <Route exact path="/contact" element={ <Contact/> } />
+      <Route exact path="/abilities" element={ <Abilities/> } />
+      <Route path="*" element={ <NotFound/> } />
+    </Routes>
+  );
+}
+
+export default App;
