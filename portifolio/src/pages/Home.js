8b0760902d52971b054e64d62bdb17db3ddf2e3e
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-05 22:10:54 -0300

    inicia projeto react

---
+++ portifolio/src/pages/Home.js
@@ -0,0 +1,215 @@
+import React from 'react';
+import Header from '../components/Header';
+import '../style/home.css';
+
+function Home() {
+  return (
+    <div>
+      <Header/>
+      <section>
+        <div class="row">
+          <div>
+            <i class="fa fa-thermometer-empty" aria-hidden="true"></i>
+            <i class="fa fa-ravelry" aria-hidden="true"></i>
+            <i class="fa fa-snowflake-o" aria-hidden="true"></i>
+            <i class="fa fa-birthday-cake" aria-hidden="true"></i>
+            <i class="fa fa-bus" aria-hidden="true"></i>
+            <i class="fa fa-credit-card" aria-hidden="true"></i>
+            <i class="fa fa-gamepad" aria-hidden="true"></i>
+            <i class="fa fa-futbol-o" aria-hidden="true"></i>
+            <i class="fa fa-file-image-o" aria-hidden="true"></i>
+            <i class="fa fa-cc-paypal" aria-hidden="true"></i>
+            <i class="fa fa-money" aria-hidden="true"></i>
+            <i class="fa fa-paypal" aria-hidden="true"></i>
+            <i class="fa fa-wheelchair-alt" aria-hidden="true"></i>
+            <i class="fa fa-hand-o-right" aria-hidden="true"></i>
+            <i class="fa fa-bicycle" aria-hidden="true"></i>
+            <i class="fa fa-sign-language" aria-hidden="true"></i>
+            <i class="fa fa-shower" aria-hidden="true"></i>
+          </div>
+          <div>
+            <i class="fa fa-thermometer-empty" aria-hidden="true"></i>
+            <i class="fa fa-ravelry" aria-hidden="true"></i>
+            <i class="fa fa-snowflake-o" aria-hidden="true"></i>
+            <i class="fa fa-birthday-cake" aria-hidden="true"></i>
+            <i class="fa fa-bus" aria-hidden="true"></i>
+            <i class="fa fa-credit-card" aria-hidden="true"></i>
+            <i class="fa fa-gamepad" aria-hidden="true"></i>
+            <i class="fa fa-futbol-o" aria-hidden="true"></i>
+            <i class="fa fa-file-image-o" aria-hidden="true"></i>
+            <i class="fa fa-cc-paypal" aria-hidden="true"></i>
+            <i class="fa fa-money" aria-hidden="true"></i>
+            <i class="fa fa-paypal" aria-hidden="true"></i>
+            <i class="fa fa-wheelchair-alt" aria-hidden="true"></i>
+            <i class="fa fa-hand-o-right" aria-hidden="true"></i>
+            <i class="fa fa-bicycle" aria-hidden="true"></i>
+            <i class="fa fa-sign-language" aria-hidden="true"></i>
+            <i class="fa fa-shower" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="row">
+          <div>
+            <i class="fa fa-thermometer-empty" aria-hidden="true"></i>
+            <i class="fa fa-ravelry" aria-hidden="true"></i>
+            <i class="fa fa-snowflake-o" aria-hidden="true"></i>
+            <i class="fa fa-birthday-cake" aria-hidden="true"></i>
+            <i class="fa fa-bus" aria-hidden="true"></i>
+            <i class="fa fa-credit-card" aria-hidden="true"></i>
+            <i class="fa fa-gamepad" aria-hidden="true"></i>
+            <i class="fa fa-futbol-o" aria-hidden="true"></i>
+            <i class="fa fa-file-image-o" aria-hidden="true"></i>
+            <i class="fa fa-cc-paypal" aria-hidden="true"></i>
+            <i class="fa fa-money" aria-hidden="true"></i>
+            <i class="fa fa-paypal" aria-hidden="true"></i>
+            <i class="fa fa-wheelchair-alt" aria-hidden="true"></i>
+            <i class="fa fa-hand-o-right" aria-hidden="true"></i>
+            <i class="fa fa-bicycle" aria-hidden="true"></i>
+            <i class="fa fa-sign-language" aria-hidden="true"></i>
+            <i class="fa fa-shower" aria-hidden="true"></i>
+          </div>
+          <div>
+            <i class="fa fa-thermometer-empty" aria-hidden="true"></i>
+            <i class="fa fa-ravelry" aria-hidden="true"></i>
+            <i class="fa fa-snowflake-o" aria-hidden="true"></i>
+            <i class="fa fa-birthday-cake" aria-hidden="true"></i>
+            <i class="fa fa-bus" aria-hidden="true"></i>
+            <i class="fa fa-credit-card" aria-hidden="true"></i>
+            <i class="fa fa-gamepad" aria-hidden="true"></i>
+            <i class="fa fa-futbol-o" aria-hidden="true"></i>
+            <i class="fa fa-file-image-o" aria-hidden="true"></i>
+            <i class="fa fa-cc-paypal" aria-hidden="true"></i>
+            <i class="fa fa-money" aria-hidden="true"></i>
+            <i class="fa fa-paypal" aria-hidden="true"></i>
+            <i class="fa fa-wheelchair-alt" aria-hidden="true"></i>
+            <i class="fa fa-hand-o-right" aria-hidden="true"></i>
+            <i class="fa fa-bicycle" aria-hidden="true"></i>
+            <i class="fa fa-sign-language" aria-hidden="true"></i>
+            <i class="fa fa-shower" aria-hidden="true"></i>
+          </div>
+        </div> 
+        <div class="row">
+          <div>
+            <i class="fa fa-thermometer-empty" aria-hidden="true"></i>
+            <i class="fa fa-ravelry" aria-hidden="true"></i>
+            <i class="fa fa-snowflake-o" aria-hidden="true"></i>
+            <i class="fa fa-birthday-cake" aria-hidden="true"></i>
+            <i class="fa fa-bus" aria-hidden="true"></i>
+            <i class="fa fa-credit-card" aria-hidden="true"></i>
+            <i class="fa fa-gamepad" aria-hidden="true"></i>
+            <i class="fa fa-futbol-o" aria-hidden="true"></i>
+            <i class="fa fa-file-image-o" aria-hidden="true"></i>
+            <i class="fa fa-cc-paypal" aria-hidden="true"></i>
+            <i class="fa fa-money" aria-hidden="true"></i>
+            <i class="fa fa-paypal" aria-hidden="true"></i>
+            <i class="fa fa-wheelchair-alt" aria-hidden="true"></i>
+            <i class="fa fa-hand-o-right" aria-hidden="true"></i>
+            <i class="fa fa-bicycle" aria-hidden="true"></i>
+            <i class="fa fa-sign-language" aria-hidden="true"></i>
+            <i class="fa fa-shower" aria-hidden="true"></i>
+          </div>
+          <div>
+            <i class="fa fa-thermometer-empty" aria-hidden="true"></i>
+            <i class="fa fa-ravelry" aria-hidden="true"></i>
+            <i class="fa fa-snowflake-o" aria-hidden="true"></i>
+            <i class="fa fa-birthday-cake" aria-hidden="true"></i>
+            <i class="fa fa-bus" aria-hidden="true"></i>
+            <i class="fa fa-credit-card" aria-hidden="true"></i>
+            <i class="fa fa-gamepad" aria-hidden="true"></i>
+            <i class="fa fa-futbol-o" aria-hidden="true"></i>
+            <i class="fa fa-file-image-o" aria-hidden="true"></i>
+            <i class="fa fa-cc-paypal" aria-hidden="true"></i>
+            <i class="fa fa-money" aria-hidden="true"></i>
+            <i class="fa fa-paypal" aria-hidden="true"></i>
+            <i class="fa fa-wheelchair-alt" aria-hidden="true"></i>
+            <i class="fa fa-hand-o-right" aria-hidden="true"></i>
+            <i class="fa fa-bicycle" aria-hidden="true"></i>
+            <i class="fa fa-sign-language" aria-hidden="true"></i>
+            <i class="fa fa-shower" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="row">
+          <div>
+            <i class="fa fa-thermometer-empty" aria-hidden="true"></i>
+            <i class="fa fa-ravelry" aria-hidden="true"></i>
+            <i class="fa fa-snowflake-o" aria-hidden="true"></i>
+            <i class="fa fa-birthday-cake" aria-hidden="true"></i>
+            <i class="fa fa-bus" aria-hidden="true"></i>
+            <i class="fa fa-credit-card" aria-hidden="true"></i>
+            <i class="fa fa-gamepad" aria-hidden="true"></i>
+            <i class="fa fa-futbol-o" aria-hidden="true"></i>
+            <i class="fa fa-file-image-o" aria-hidden="true"></i>
+            <i class="fa fa-cc-paypal" aria-hidden="true"></i>
+            <i class="fa fa-money" aria-hidden="true"></i>
+            <i class="fa fa-paypal" aria-hidden="true"></i>
+            <i class="fa fa-wheelchair-alt" aria-hidden="true"></i>
+            <i class="fa fa-hand-o-right" aria-hidden="true"></i>
+            <i class="fa fa-bicycle" aria-hidden="true"></i>
+            <i class="fa fa-sign-language" aria-hidden="true"></i>
+            <i class="fa fa-shower" aria-hidden="true"></i>
+          </div>
+          <div>
+            <i class="fa fa-thermometer-empty" aria-hidden="true"></i>
+            <i class="fa fa-ravelry" aria-hidden="true"></i>
+            <i class="fa fa-snowflake-o" aria-hidden="true"></i>
+            <i class="fa fa-birthday-cake" aria-hidden="true"></i>
+            <i class="fa fa-bus" aria-hidden="true"></i>
+            <i class="fa fa-credit-card" aria-hidden="true"></i>
+            <i class="fa fa-gamepad" aria-hidden="true"></i>
+            <i class="fa fa-futbol-o" aria-hidden="true"></i>
+            <i class="fa fa-file-image-o" aria-hidden="true"></i>
+            <i class="fa fa-cc-paypal" aria-hidden="true"></i>
+            <i class="fa fa-money" aria-hidden="true"></i>
+            <i class="fa fa-paypal" aria-hidden="true"></i>
+            <i class="fa fa-wheelchair-alt" aria-hidden="true"></i>
+            <i class="fa fa-hand-o-right" aria-hidden="true"></i>
+            <i class="fa fa-bicycle" aria-hidden="true"></i>
+            <i class="fa fa-sign-language" aria-hidden="true"></i>
+            <i class="fa fa-shower" aria-hidden="true"></i>
+          </div>
+          </div>
+          <div class="row">
+          <div>
+            <i class="fa fa-thermometer-empty" aria-hidden="true"></i>
+            <i class="fa fa-ravelry" aria-hidden="true"></i>
+            <i class="fa fa-snowflake-o" aria-hidden="true"></i>
+            <i class="fa fa-birthday-cake" aria-hidden="true"></i>
+            <i class="fa fa-bus" aria-hidden="true"></i>
+            <i class="fa fa-credit-card" aria-hidden="true"></i>
+            <i class="fa fa-gamepad" aria-hidden="true"></i>
+            <i class="fa fa-futbol-o" aria-hidden="true"></i>
+            <i class="fa fa-file-image-o" aria-hidden="true"></i>
+            <i class="fa fa-cc-paypal" aria-hidden="true"></i>
+            <i class="fa fa-money" aria-hidden="true"></i>
+            <i class="fa fa-paypal" aria-hidden="true"></i>
+            <i class="fa fa-wheelchair-alt" aria-hidden="true"></i>
+            <i class="fa fa-hand-o-right" aria-hidden="true"></i>
+            <i class="fa fa-bicycle" aria-hidden="true"></i>
+            <i class="fa fa-sign-language" aria-hidden="true"></i>
+            <i class="fa fa-shower" aria-hidden="true"></i>
+          </div>
+            <div>
+              <i class="fa fa-thermometer-empty" aria-hidden="true"></i>
+              <i class="fa fa-ravelry" aria-hidden="true"></i>
+              <i class="fa fa-snowflake-o" aria-hidden="true"></i>
+              <i class="fa fa-birthday-cake" aria-hidden="true"></i>
+              <i class="fa fa-bus" aria-hidden="true"></i>
+              <i class="fa fa-credit-card" aria-hidden="true"></i>
+              <i class="fa fa-gamepad" aria-hidden="true"></i>
+              <i class="fa fa-futbol-o" aria-hidden="true"></i>
+              <i class="fa fa-file-image-o" aria-hidden="true"></i>
+              <i class="fa fa-cc-paypal" aria-hidden="true"></i>
+              <i class="fa fa-money" aria-hidden="true"></i>
+              <i class="fa fa-paypal" aria-hidden="true"></i>
+              <i class="fa fa-wheelchair-alt" aria-hidden="true"></i>
+              <i class="fa fa-hand-o-right" aria-hidden="true"></i>
+              <i class="fa fa-bicycle" aria-hidden="true"></i>
+              <i class="fa fa-sign-language" aria-hidden="true"></i>
+              <i class="fa fa-shower" aria-hidden="true"></i>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Home;
