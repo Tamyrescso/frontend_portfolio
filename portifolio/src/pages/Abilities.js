fb283ff4ad7e3ef25c006ea5c110cfe6d0c621bd
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-20 17:47:09 -0300

    add lógica do componente abilities

--- portifolio/src/pages/Abilities.js
+++ portifolio/src/pages/Abilities.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import {
+  css,
+  github,
+  html,
+  JS,
+  mySQL,
+  node,
+  react,
+  redux
+} from '../images/skills/index.js';
+import '../style/abilities.css';
 
 function Abilities() {
   return (
-    <div>Abilities</div>
+    <div className='abilities'>
+        <Header/>
+        <div className='mainAbilities'>
+          <img id='cssCard' src={ css } alt='css logo and a ingredient' />
+  
+          <img id='githubCard' src={ github } alt='github logo and a ingredient' />
+    
+          <img id='htmlCard' src={ html } alt='html logo and a ingredient' />
+    
+          <img id='JSCard' src={ JS } alt='JS logo and a ingredient' />
+  
+          <img id='mysqlCard' src={ mySQL } alt='mysql logo and a ingredient' />
+      
+          <img id='nodeCard' src={ node } alt='node logo and a ingredient' />
+      
+          <img id='reactCard' src={ react } alt='react logo and a ingredient' />
+      
+          <img id='reduxCard' src={ redux } alt='redux logo and a ingredient' />
+        </div>
+        <Footer/>
+    </div>
   )
 }
 
-export default Abilities
+export default Abilities;
