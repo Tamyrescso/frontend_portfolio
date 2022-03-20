25e3938fd23493983daa53c8ca8ba9b2f39d8d8c
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-20 17:55:23 -0300

    add header e footer na página projects

--- portifolio/src/pages/Projects.js
+++ portifolio/src/pages/Projects.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div>
+      <Header/>
+      <div>Projects</div>
+      <Footer/>
+    </div>
   )
 }
 
