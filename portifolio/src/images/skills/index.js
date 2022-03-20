af86f4b0229a51f4961e95e1d60432dd02d12c51
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-20 17:49:22 -0300

    cria arquivo para exportar imagens de habilidades

---
+++ portifolio/src/images/skills/index.js
@@ -0,0 +1,19 @@
+import css from './css.png';
+import github from './github.png';
+import html from './html.png';
+import JS from './JS.png';
+import mySQL from './mySQL.png';
+import node from './node.png';
+import react from './react.png';
+import redux from './redux.png';
+
+export {
+  css,
+  github,
+  html,
+  JS,
+  mySQL,
+  node,
+  react,
+  redux
+};
