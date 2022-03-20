fc4eaf004377167404a5b30bf37bacddd3927247
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-20 17:56:29 -0300

    add state de mostrar menu hambúrguer

--- portifolio/src/context/PortfolioProvider.js
+++ portifolio/src/context/PortfolioProvider.js
@@ -3,10 +3,14 @@ import PortfolioContext from './PortfolioContext';
 
 function PortfolioProvider({ children }) {
   const [language, setLanguage] = useState('pt');
+  const [showMenu, setShowMenu] = useState(false);
+
 
   const values = {
     setLanguage,
     language,
+    setShowMenu,
+    showMenu,
   }
   return (
     <PortfolioContext.Provider value={ values }>
