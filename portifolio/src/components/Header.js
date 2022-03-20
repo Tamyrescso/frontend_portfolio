4205094e8cfa8244de573929e109a9610169d5ad
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-20 17:57:30 -0300

    incrementa lógica de linguagem e acrescenta menu burguer

--- portifolio/src/components/Header.js
+++ portifolio/src/components/Header.js
@@ -2,26 +2,44 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import darkLogo from '../images/logo_dark.svg';
 import PortfolioContext from '../context/PortfolioContext';
+import brazil from '../images/brasil.png';
+import usa from '../images/eua.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import MenuBurguer from './MenuBurguer';
 import '../style/header.css';
 
 function Header() {
-  const { setLanguage, language } = useContext(PortfolioContext);
+  const {
+    setLanguage,
+    language,
+    setShowMenu,
+    showMenu } = useContext(PortfolioContext);
 
   function changeLanguage(e) {
-    const { value } = e.target;
-    setLanguage(value);
+    const { id } = e.target;
+    setLanguage(id);
   }
 
   const ptHeader = (
     <nav className='navBar'>
       <Link to="/projects">
-        <h3>Projetos</h3>
+        <button>
+          <span className='tech'>Projetos</span>
+          <span className='food'>Receitas</span>
+        </button>
       </Link>
       <Link to="/abilities">
-        <h3>Habilidades</h3>
+        <button>
+          <span className='tech'>Habilidades</span>
+          <span className='food'>Ingredientes</span>
+        </button>
       </Link>
       <Link to="/contact">
-        <h3>Contato</h3>
+        <button>
+          <span className='tech'>Contato</span>
+          <span className='food'>Reserva</span>
+        </button>
       </Link>
     </nav>
   );
@@ -29,27 +47,49 @@ function Header() {
   const enHeader = (
     <nav className='navBar'>
       <Link to="/projects">
-        <h3>Projects</h3>
+        <button>
+          <span className='tech'>Projects</span>
+          <span className='food'>Recipes</span>
+        </button>
       </Link>
       <Link to="/abilities">
-        <h3>Abilities</h3>
+        <button>
+          <span className='tech'>Skills</span>
+          <span className='food'>Ingredients</span>
+        </button>
       </Link>
       <Link to="/contact">
-        <h3>Contact</h3>
+        <button>
+          <span className='tech'>Contact</span>
+          <span className='food'>Reservation</span>
+        </button>
       </Link>
     </nav>
   );
 
   return (
     <div className='headerBar'>
+      { showMenu 
+        ? (
+          <button type='button' className='exitBtn' onClick={() => setShowMenu(false)}>
+              <FontAwesomeIcon  icon={faXmark} />
+            </button>
+        )
+        : (
+            <button type='button' className='menuBtn' onClick={() => setShowMenu(true)}>
+              <FontAwesomeIcon icon={faBars} />
+            </button>
+        )
+      }
+      { showMenu && <MenuBurguer /> }
       <Link to="/">
         <img className='logo' src={darkLogo} alt="logo"/>
       </Link>
       {language === 'pt'? ptHeader : enHeader}
-      <select onChange={ changeLanguage } className='languages'>
-        <option value='pt'>PT-BR</option>
-        <option value='en'>EN-EUA</option>
-      </select>
+      <div className='languages'>
+        <img onClick={ changeLanguage } id='pt' className='brazil' src={ brazil } alt='brazil flag'/>
+        <img onClick={ changeLanguage } id='en' className='usa' src={ usa } alt='usa flag'/>
+      </div>
     </div>
   )
 }
