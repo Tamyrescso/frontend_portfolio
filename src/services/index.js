90cb42703b352836a3c41b064ddd1e3a0bc06caa
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-28 02:39:59 -0300

    conserta nome da chave para req post

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const sendMessage = async ({ name, email, message}) => {
+const sendMessage = async ({ name, email, message: content}) => {
   const request = axios
     .post(process.env.REACT_APP_DATABASE_URL, {
       name,
       email,
-      message
+      content, 
     })
   const response = await request;
   return response;
