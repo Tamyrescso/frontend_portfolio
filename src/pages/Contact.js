f31231cc290458132aaaee599a998d52016b576e
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-28 02:16:13 -0300

    add contactform na página contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import ContactForm from '../components/ContactForm';
 import '../style/contact.css';
 
 function Contact() {
   return (
     <div>
       <Header/>
-      <div className='mainContact'></div>
+      <div className='mainContact'>
+        <ContactForm />
+      </div>
       <Footer/>
     </div>
   )
 }
 
-export default Contact
+export default Contact;
