f2a7bd11cc783e5b7fbfa66bdd7b807539141057
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-30 23:45:26 -0300

    index para exportar gifs

--- src/images/projects/index.js
+++ src/images/projects/index.js
@@ -1,5 +1,19 @@
-import starwars from './starwars.png';
+import starwars from './starwars.gif';
+import recipes_app from './recipes_app.gif';
+import trybewallet from './trybewallet.gif';
+import tryunfo from './tryunfo.gif';
+import todo from './todo.gif';
+import zoo from './zoo.gif';
+import store from './store.gif';
+import blog_posts from './blog_posts.gif';
 
 export {
   starwars,
+  recipes_app,
+  trybewallet,
+  tryunfo,
+  todo,
+  zoo,
+  store,
+  blog_posts,
 };
