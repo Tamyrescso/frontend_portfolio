0a9516d95e6f1e57071604a18f47f16c81fc3693
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-30 23:44:10 -0300

    alterações na page contact

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import validationForm from '../utils/validations';
 import sendMessage from '../services';
+import PortfolioContext from '../context/PortfolioContext';
 import '../style/contactForm.css';
 
 function ContactForm() {
@@ -9,6 +10,7 @@ function ContactForm() {
     email: '',
     message: ''
   });
+  const { language } = useContext(PortfolioContext);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -26,20 +28,40 @@ function ContactForm() {
     const { message } = data;
     return alert(message);
   }
+
+  const ptForm = (
+    <form> 
+      <label htmlFor="name">Nome</label>
+      <input type="text" id="name" name="name" onChange={handleChange} placeholder="Escreva seu nome ex.: Ada Lovelace" />
+
+      <label htmlFor="email">E-mail</label>
+      <input type="text" id="email" name="email" onChange={handleChange} placeholder="Escreva seu e-mail ex.: email@email.com" />
+
+      <label htmlFor="message">Mensagem</label>
+      <textarea id="message" name="message" onChange={handleChange} placeholder="Fique à vontade para falar do meu portifólio aqui! me mande um comentário, elogio, dica, me chame para um projeto ou até me mande uma receita :)" className="textArea"></textarea>
+
+      <input type="submit" value="Enviar" onClick={handleClick} />
+    </form>
+  );
+
+  const enForm = (
+    <form> 
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" name="name" onChange={handleChange} placeholder="Write your name ex.: Ada Lovelace" />
+
+      <label htmlFor="email">E-mail</label>
+      <input type="text" id="email" name="email" onChange={handleChange} placeholder="Write your email ex.: email@email.com" />
+
+      <label htmlFor="message">Message</label>
+      <textarea id="message" name="message" onChange={handleChange} placeholder="Be comfortable to talk about my portfolio here! send me a comment, a compliment, a tip, call me to work in a project or even send me a recipe :)" className="textArea"></textarea>
+
+      <input type="submit" value="Send" onClick={handleClick} />
+    </form>
+  )
+
   return (
     <div className='formContact'>
-      <form> 
-        <label htmlFor="name">Nome</label>
-        <input type="text" id="name" name="name" onChange={handleChange} placeholder="Escreva seu nome" />
-
-        <label htmlFor="email">E-mail</label>
-        <input type="text" id="email" name="email" onChange={handleChange} placeholder="email@email.com" />
-
-        <label htmlFor="message">Mensagem</label>
-        <textarea id="message" name="message" onChange={handleChange} placeholder="Fique à vontade para falar do meu portifólio! me mande um comentário, elogio, dica, me chame para um projeto ou até me mande uma receita :)" className="textArea"></textarea>
-
-        <input type="submit" value="Enviar" onClick={handleClick} />
-      </form>
+      { language === 'pt'? ptForm : enForm }
     </div>
   )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ContactForm from '../components/ContactForm';
+import PortfolioContext from '../context/PortfolioContext';
 import '../style/contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab, faGithub, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab);
 
 function Contact() {
+  const { language } = useContext(PortfolioContext);
+
+  const ptTitle = (
+    <h2 className='linkTitle'>Contato</h2>
+  );
+
+  const enTitle = (
+    <h2 className='linkTitle'>Contact</h2>
+  );
   return (
-    <div>
+    <div className='contactPage'>
       <Header/>
       <div className='mainContact'>
+        {language === 'pt'? ptTitle : enTitle}
         <ContactForm />
+        <div className='containerLinks'>
+          <FontAwesomeIcon className='myLinks github' icon={faGithub}/>
+          <FontAwesomeIcon className='myLinks facebook' icon={faFacebook}/>
+          <FontAwesomeIcon className='myLinks linkedin' icon={faLinkedin}/>
+        </div>
       </div>
       <Footer/>
     </div>
