b0c64cb74603debc521fe41d9d674dfaeee01401
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-04-01 15:08:36 -0300

    add código e estilização da page not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import notFound from '../images/notFound.png';
+import '../style/notFound.css';
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='notFoundWrapper'>
+      <img className='notFoundImg' src={notFound} alt="a figure of woman searching and not found something inside of a refrigerator, next to her there is a figure of a magnifying glass and the text '404 NOT FOUND' inside it"/>
+    </div>
   )
 }
 
