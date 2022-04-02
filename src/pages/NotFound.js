0218e39666457f428f33e278fda824fa3a1108d3
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-04-02 00:41:58 -0300

    altera imagem notFound e add botão home

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -7,8 +7,11 @@ function NotFound() {
   return (
     <div className='notFoundWrapper'>
       <img className='notFoundImg' src={notFound} alt="a figure of woman searching and not found something inside of a refrigerator, next to her there is a figure of a magnifying glass and the text '404 NOT FOUND' inside it"/>
+      <Link to='/' className='backHomeLink'>
+        <button className='backHome'>Home</button>
+      </Link>
     </div>
   )
 }
 
-export default NotFound
+export default NotFound;
