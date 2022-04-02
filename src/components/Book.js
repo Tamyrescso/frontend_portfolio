6522f5204ec03dd84986d0da7722f97fa4d2988a
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-04-02 20:09:23 -0300

    atualiza seção about me

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,6 +4,7 @@ import arabesque from '../images/arabesco_book.svg';
 import me from '../images/me.png';
 import darkMe from '../images/darkMe.png';
 import PortfolioContext from '../context/PortfolioContext';
+import { aboutMePt, aboutMeEn } from '../data/aboutMe';
 
 function Book() {
   const [bookClass, setBookClass] = useState('book');
@@ -12,28 +13,6 @@ function Book() {
   const changeBookClass = () => {
     bookClass === 'book' ? setBookClass('book bookSelected') : setBookClass('book');
   }
-
-  const about = (
-    <>
-      <h2 className='aboutTitle'>About me</h2>
-      <p className='aboutText'>
-        {`I'm passionate about food and technology!
-        I graduated in Gastronomy at UFRJ and I have more than 3 years of experience in the procurement field. While I was working in that area, I realized I haven't found my place yet.
-        So, in 2021, finally I'd got the courage, and, with a lot of planning and conscious, I've started my career transition at Trybe. There, I've just completed my front-end cycle and nowadays I study back-end.`}
-      </p>
-    </>
-  )
-
-  const sobre = (
-    <>
-      <h2 className='aboutTitle'>Sobre mim</h2>
-      <p className='aboutText'>
-            {`Sou uma pessoa apaixonada pela área da tecnologia e da alimentação!
-            Me formei em Gastronomia pela UFRJ e tenho mais de 3 anos de experiência na área de compras, nesse tempo eu percebi que ainda não havia achado o meu lugar.
-            Em 2021, finalmente tomei coragem, e com muito planejamento e consciência, comecei minha transição de carreira na Trybe, onde me formei como desenvolvedora Front-end e atualmente estudo Back-end.`}
-      </p>
-    </>
-  )
 
   const cover = (
     <h1 className='coverText'>Tamyres'<br></br><s>recipes book</s><br></br>Portfolio</h1>
@@ -49,7 +28,7 @@ function Book() {
       <div className={ darkTheme? "page6 page6Dark" : "page6" }></div>
       <div className={ darkTheme? "page5 page5Dark" : "page5" }></div>
       <div className={ darkTheme? "page4 page4Dark" : "page4" }>
-        {language === 'pt'? sobre : about}
+        { language === 'pt'? aboutMePt : aboutMeEn }
       </div>
       <div className={ darkTheme? "page3 page3Dark" : "page3" }></div>
       <div className={ darkTheme? "page2 page2Dark" : "page2" }></div>
---
+++ src/data/aboutMe.js
@@ -0,0 +1,27 @@
+const aboutMeEn = (
+  <>
+      <h2 className='aboutTitle'>About me</h2>
+      <p className='aboutText'>I'm passionate about tech and food!</p>
+      <p className='aboutText'>I consider myself an eternal student, I love <b>planning and researching</b> solutions, addicted to to-do lists, <b>restless</b> by nature and extremely <b>focused</b> on my goals.</p>
+      <p className='aboutText'>I'm carioca, I have a<b>degree in gastronomy</b> from UFRJ and I have been a buyer for over 3 years.</p>
+      <p className='aboutText'>I was <b>unsatisfied</b> with my professional course and because of that I kept looking for <b>self knowledge</b> and new paths. Until one day in may, in the middle of my vacation, I  came across <b>Trybe</b> and I had an epiphany!</p>
+      <p className='aboutText'><b>HOW I NEVER THOUGHT ABOUT THIS BEFORE?!</b></p>
+      <p className='aboutText'>And so, with plenty of research, planning and <b>consciousness</b>, I quit my job and started on this journey that seems to have been handmade for me!</p>
+      <p className='aboutText'>Today, after almost 1 year, <b>Front End and future Full Stack developer</b>, I'm sure the career transition was the best decision I've ever made!</p>
+    </>
+)
+
+const aboutMePt = (
+  <>
+      <h2 className='aboutTitle'>Sobre mim</h2>
+      <p className='aboutText'>Sou uma pessoa apaixonada pela tecnologia e alimentação!</p>
+      <p className='aboutText'>Me considero uma eterna estudante, amo <b>planejar e pesquisar</b> soluções, viciada em to-do lists, <b>inquieta</b> por natureza e extremamente <b>focada</b> em meus objetivos.</p>
+      <p className='aboutText'>Sou carioca, <b>bacharel em gastronomia</b> pela UFRJ e fui compradora por mais de 3 anos.</p>
+      <p className='aboutText'>Estava <b>insatisfeita</b> com meu rumo profissional e por isso continuei na busca pelo <b>autoconhecimento</b> e novos caminhos. Até que, num dia de férias em maio, eu topei com a <b>Trybe</b> e tive uma epifania!</p>
+      <p className='aboutText'><b>COMO NUNCA PENSEI NISSO ANTES?!</b></p>
+      <p className='aboutText'>E assim, com muita pesquisa, planejamento e <b>consciência</b>, eu larguei meu emprego e embarquei nessa jornada que parece que foi feita a mão pra mim!</p>
+      <p className='aboutText'>Hoje, depois de quase 1 ano, <b>desenvolvedora Front End e futura FullStack</b>, tenho certeza que a transição de carreira foi a melhor decisão que já tomei!</p>
+    </>
+)
+
+export { aboutMeEn, aboutMePt }
